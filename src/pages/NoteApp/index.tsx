528e864d0744baf2f50175890e26dfaa67fd0b14
ww_side <daniil.metelia@gmail.com>
2023-07-29 09:53:01 +0300

    added Layout for NoteApp

--- src/pages/NoteApp/index.tsx
+++ src/pages/NoteApp/index.tsx
@@ -3,18 +3,19 @@ import NoteTable from '../../components/shared/NoteTable';
 import Button from '../../components/common/Button';
 import ModalAddNote from '../../components/shared/ModalAddNote';
 import SummaryTable from '../../components/shared/SummaryTable';
+import Layout from '../../components/common/Layout';
 
 const NoteApp = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <>
+    <Layout>
       <NoteTable />
       <Button color="blue" onClick={() => setIsModalOpen(!isModalOpen)}>
         Add a new note
       </Button>
       <ModalAddNote isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
       <SummaryTable />
-    </>
+    </Layout>
   );
 };
 
