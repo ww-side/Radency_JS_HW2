eb7528c3d1d97774c7e525455c66f818b312e56c
ww_side <daniil.metelia@gmail.com>
2023-07-29 16:13:18 +0300

    added Layout to NoteApp

--- src/pages/NoteApp/index.tsx
+++ src/pages/NoteApp/index.tsx
@@ -1,20 +1,24 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import NoteTable from '../../components/shared/NoteTable';
 import Button from '../../components/common/Button';
+import Layout from '../../components/common/Layout';
 import ModalAddNote from '../../components/shared/ModalAddNote';
 import SummaryTable from '../../components/shared/SummaryTable';
-import Layout from '../../components/common/Layout';
+import ArchiveTable from '../../components/shared/ArchiveTable';
 
-const NoteApp = () => {
+const NoteApp: FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <Layout>
       <NoteTable />
-      <Button color="blue" onClick={() => setIsModalOpen(!isModalOpen)}>
-        Add a new note
-      </Button>
+      <span className="flex justify-end">
+        <Button color="blue" onClick={() => setIsModalOpen(!isModalOpen)}>
+          Add a new note
+        </Button>
+      </span>
       <ModalAddNote isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
       <SummaryTable />
+      <ArchiveTable />
     </Layout>
   );
 };
