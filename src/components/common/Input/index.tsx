2ae418e0fe756c5b6148413277d95ccc516f497a
ww_side <daniil.metelia@gmail.com>
2023-07-29 16:08:23 +0300

    added Input and Select component

---
+++ src/components/common/Input/index.tsx
@@ -0,0 +1,24 @@
+import { ChangeEvent, FC } from 'react';
+
+interface InputProps {
+  type: string;
+  value: string;
+  placeholder: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: FC<InputProps> = ({ type, value, placeholder, onChange }) => {
+  return (
+    <input
+      type={type}
+      value={value}
+      className="appearance-none border rounded w-full py-2 px-3
+      text-gray-700 leading-tight focus:outline-none
+      focus:shadow-outline"
+      placeholder={placeholder}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Input;
---
+++ src/components/common/Select/index.tsx
@@ -0,0 +1,27 @@
+import { ChangeEvent, FC } from 'react';
+import { Category } from '../../../models/category.ts';
+
+interface SelectProps {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const Select: FC<SelectProps> = ({ value, onChange }) => {
+  return (
+    <select
+      value={value}
+      onChange={onChange}
+      className="block text-gray-700 appearance-none w-full
+      bg-white border border-gray-300 rounded py-2 px-3
+      leading-tight focus:outline-none focus:shadow-outline"
+    >
+      {Object.values(Category).map(categoryValue => (
+        <option key={categoryValue} value={categoryValue}>
+          {categoryValue}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
