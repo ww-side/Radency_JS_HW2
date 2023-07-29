ad0cacf6aeeb080bf39d8354349bad4d3f3ac351
ww_side <daniil.metelia@gmail.com>
2023-07-29 16:27:32 +0300

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import NoteApp from './pages/NoteApp';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NoteApp from './components/pages/NoteApp';
+import ErrorPage from './components/pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      {
+        path: '/',
+        element: <NoteApp />,
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  },
+]);
 
 function App() {
-  return <NoteApp />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
